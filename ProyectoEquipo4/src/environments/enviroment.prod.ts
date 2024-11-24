7e46a420f4d2790653efbd6858773f8593e0f65e
Yuliana <yulianavidal198@gmail.com>
2024-11-24 18:40:09 -0500

    Correcciones
    Correcciones

---
+++ ProyectoEquipo4/src/environments/enviroment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: true,
+    firebase: {
+      projectId: 'plural-coffe',
+      appId: '1:470356965635:web:ae70ba87b9bf448dd2d136',
+      storageBucket: 'plural-coffe.appspot.com',
+      apiKey: 'AIzaSyBj4GRs00ok0LTsWpUaM1mCRq_CmKcI1gY',
+      authDomain: 'plural-coffe.firebaseapp.com',
+      messagingSenderId: '470356965635',
+      measurementId: 'G-DBQT2G5DNJ',
+    },
+    apiUrl: 'https://backend-equipo4.onrender.com' // URL del backend en producción
+  };
+  
